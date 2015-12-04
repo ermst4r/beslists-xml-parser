f514fa51ea64f95c6c03c80f2efedb3418d815bf
ermst4r <erm@live.nl>
2015-12-04 15:40:50 +0100

    fixed the xml feed

--- services/Generatexml.js
+++ services/Generatexml.js
@@ -11,30 +11,24 @@ var Generatexml = function () {
 
 
     this.productXmlOutput = function (connection) {
-
         async.series([
 
+            // first load this
             function(callback){
                 var sql    = "SELECT * FROM orders As o LEFT JOIN payment As p ON o.order_number = p.fk_order_number LEFT JOIN customers As c ON o.order_number = c.fk_order_number";
                 connection.query(sql, function(err, rows, fields) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        callback(null,rows);
-
-                    }
+                    if (err) return next(err);
+                    callback(null,rows);
                 });
-            }, function (callback) {
-                var sqlCheckCodeExists    = "SELECT *   FROM feed_data ";
+            },
+            // then this
+            function (callback) {
+                var sqlCheckCodeExists    = "SELECT *   FROM feed_data";
                 connection.query(sqlCheckCodeExists, function(codeErr, codeRows, codeFields) {
                     if (codeErr) throw codeErr;
                     callback(null,codeRows);
                 });
             }
-
-
-
-
 
 
         ], function(error, results) {
@@ -45,7 +39,7 @@ var Generatexml = function () {
                 var xml = builder.create('root');
                 var totalShopAmount = parseFloat(item.price) + parseFloat(item.shipping);
                 totalShopAmount = Math.round(totalShopAmount * 100) / 100;
-
+                // callback find ordernumber
                 var timestamp = new Date(item.date_created);
                 var plainDate = timestamp.getUTCFullYear() + '-' +
                     ('00' + (timestamp.getUTCMonth()+1)).slice(-2) + '-' +
@@ -53,9 +47,11 @@ var Generatexml = function () {
                 xml.ele('customer_nr',item.customer_id);
                 xml.ele('date',plainDate);
                 xml.ele('delivery_date',plainDate);
+                xml.ele('delivery_time',plainDate);
                 xml.ele('initial_payment_date',timestamp.toISOString().slice(0, 19).replace('T', ' '));
+                xml.ele('delivery_method',57); //
                 xml.ele('delivery_floor');
-                xml.ele('payment_condition',0063); //
+                xml.ele('payment_condition',0063);
                 xml.ele('currency',0);
                 xml.ele('price_type','I');
                 xml.ele('transaction_discount',0);
@@ -69,21 +65,38 @@ var Generatexml = function () {
                 }  else if (item.payment_method=='iDEAL') {
                     xml.ele('initial_payment_method',4);
                 }
-
+                xml.ele('branch',item.branch_id);
 
                 var orderRows =  xml.ele('order_rows')
-                /* create the products */
 
+                /* create the products */
                 var productSql    = "SELECT * FROM products WHERE fk_order_number = "+ connection.escape(item.order_number);
                 connection.query(productSql, function(productErr, productRows, productFields) {
                     var counter = 1;
-
                     productRows.forEach(function(productItem) {
+
+
+                        // Get some product specs from the feed against the beslist feed
                         var wareHouseId = '';
                         var commission_code = '';
                         var zelfMontage = '';
+                        var verzendMethode = '';
+
+                        var codeRand = new Array('0112061051','9002759910792','200301951','78009230');
+                        var codeCheck = codeRand[Math.floor(Math.random() * codeRand.length) + 1];
+                        results[1].forEach(function(items) {
+                            if(items.unieke_code == codeCheck) { // change to  the product bvbcode later on
+                                wareHouseId = items.warehouse;
+                                commission_code = items.commissiecode;
+                                zelfMontage = items.zelfmontage;
+                                verzendMethode = items.verzendMethode;
+                            }
+                        });
+
+
                         var orderRow = orderRows.ele('order_row');
                         orderRow.ele('rownr',counter);
+
                         orderRow.ele('row_type','R'); // conditie toevoegen
                         orderRow.ele('article_nr',productItem.bvb_code);
                         orderRow.ele('description',productItem.product_name);
@@ -91,16 +104,8 @@ var Generatexml = function () {
                         orderRow.ele('price',productItem.price.toFixed(4).replace('.',','));
                         orderRow.ele('discount','0,0000');
                         orderRow.ele('quantity',productItem.number_orderd);
-
-                        // callback find ordernumber
-                        results[1].forEach(function(items) {
-                           if(items.unieke_code == '260105484') {
-                               wareHouseId = items.warehouse;
-                               commission_code = items.commissiecode;
-                               zelfMontage = items.zelfmontage;
-                           }
-                        });
-
+                        orderRow.ele('warehouse',wareHouseId);
+                        orderRow.ele('commission_code',commission_code);
                         if(zelfMontage == 'Ja') {
                             counter++;
                             var zelfMontageRow = orderRows.ele('order_row');
@@ -108,29 +113,23 @@ var Generatexml = function () {
                             zelfMontageRow.ele('row_type','T');
                             zelfMontageRow.ele('text_line','Zelfmontage');
                             zelfMontageRow.ele('text_line_article_bound','J');
+                            counter++;
+                        }  else {
+                            counter++;
                         }
-
-                        // increment by 1
-                        // shipping row
-                        counter++;
-                        var shippingRow = orderRows.ele('order_row');
-                        shippingRow.ele('rownr',counter);
-                        shippingRow.ele('article_nr',4);
-                        shippingRow.ele('description','verzendkosten');
-                        shippingRow.ele('warehouse',wareHouseId);
-                        shippingRow.ele('commission_code',commission_code);
-                        shippingRow.ele('price',productItem.shipping.toFixed(4).replace('.',','));
-
-                        // increment again by 1
-                        counter++;
-
                     });
 
+                    var shippingRow = orderRows.ele('order_row');
+                    shippingRow.ele('rownr',counter);
+                    shippingRow.ele('article_nr',4);
+                    shippingRow.ele('description','verzendkosten');
+                    shippingRow.ele('warehouse',5);
+                    shippingRow.ele('commission_code',1);
+                    shippingRow.ele('price',totalShopAmount.toFixed(4).replace('.',','));
                     console.log(xml.end({ pretty: true}));
 
-                    //console.log(xml.end({ pretty: true}));
                 });
-                /* create the products */
+
 
             });
 
